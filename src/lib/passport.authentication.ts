5379746cd8b03c3d61ffb1d6fecaa2f676af7dc5
Brent Van Geertruy <brentvangeertruy@outlook.com>
2017-08-19 15:25:29 +0200

    Cleanup in scripts and yarn and added some typed definitions for passport

--- src/lib/passport.authentication.ts
+++ src/lib/passport.authentication.ts
@@ -1,5 +1,5 @@
 import * as passport from 'passport';
-import * as LocalStrategy from 'passport-local';
+import { Strategy as LocalStrategy } from 'passport-local';
 import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
 
 import {
@@ -19,19 +19,22 @@ export default class PassportAuthentication extends BaseAuthentication {
    * @param {any} fn
    * @memberOf PassportAuthenticaton
    */
-  setLocalStrategy = (localConfig = DEF_LOCAL, fn) => {
+  setLocalStrategy = (localConfig = DEF_LOCAL, fn: Function) => {
     // console.log('Set a new strategy', fn);
     this.localStrategyConfig = Object.assign({}, localConfig, {
       passReqToCallback: false,
     });
 
     // Convert the callback function needed to a function returning a Promise
-    passport.use(new LocalStrategy(this.localStrategyConfig,
+    const localStrategy = new LocalStrategy(this.localStrategyConfig,
       (email, password, next: any) => {
         fn(email, password)
           .then(result => next(null, result))
           .catch(error => next(error, null));
-      }));
+      });
+
+    // Use this local strategy
+    passport.use(localStrategy);
   }
 
 
@@ -65,7 +68,7 @@ export default class PassportAuthentication extends BaseAuthentication {
    */
   getJwtToken(user) {
     if (this.jwtStrategyConfig) {
-      return createWebtoken(user, this.jwtStrategyConfig);
+      return createWebtoken(user, this.jwtStrategyConfig.secretOrKey, this.jwtStrategyConfig);
     }
     throw new Error('JWT Strategy configuration not properly set');
   }
@@ -73,19 +76,19 @@ export default class PassportAuthentication extends BaseAuthentication {
   /**
    * Authenticate depending on authentication type (local/jwt)
    *
-   * @param {any} req
+   * @param {Request} req
    * @param {string} [type='local']
    * @returns { Promise }
    *
    * @memberOf PassportAuthenticaton
    */
-  authenticate(req: any, type = 'local') {
+  authenticate(req: any, type: string = 'local') {
     return new Promise((resolve, reject) => {
       passport.authenticate(type, (error, user) => {
         if (error) return reject(error);
         else if (!user) return reject('User not found');
         return resolve(user);
-      })(req);
+      }).bind(req);
     });
   }
 }
--- src/utils/cipher.ts
+++ src/utils/cipher.ts
@@ -1,38 +1,38 @@
-import * as jsonwebtoken from 'jsonwebtoken';
+import { sign as jsonwebtokenSign, Secret, SignOptions } from 'jsonwebtoken';
 import * as bcryptNodejs from 'bcrypt-nodejs';
 
 
 /**
  * Get a hashed password
- * @param password
+ * @param {String} password
  * @return {String}
  */
-export function getHashedPassword(password: String): string {
+export function getHashedPassword(password: string): string {
   return bcryptNodejs.hashSync(password);
 }
 
 /**
  * compare user password hash with unhashed password
- * @param password
- * @param hashedPw
+ * @param {String} password
+ * @param {String} hashedPw
  * @returns {Boolean} isPasswordSame
  */
-export function comparePassword(password: String, hashedPw: String): boolean {
+export function comparePassword(password: string, hashedPw: string): boolean {
   return bcryptNodejs.compareSync(password, hashedPw);
 }
 
 /**
  * Create a new json webtoken
  * @param {Object} user
- * @param {Object} jwtSettings
+ * @param {String|Buffer} secretOrKey
+ * @param {SignOptions} jwtSettings
  * @returns {Object}
  */
-export function createWebtoken(user: any, jwtSettings: any): any {
-  return jsonwebtoken.sign({ user },
-    jwtSettings.secret, {
-      algorithm: jwtSettings.algorithm,
-      expiresIn: `${jwtSettings.expiresIn}s`, // Expires in seconds
-      issuer: jwtSettings.issuer,
-      audience: jwtSettings.audience,
-    });
+export function createWebtoken(user: Object, secretOrKey: Secret, jwtSettings: SignOptions): any {
+  return jsonwebtokenSign({ user }, secretOrKey, {
+    algorithm: jwtSettings.algorithm,
+    expiresIn: `${jwtSettings.expiresIn}s`, // Expires in seconds
+    issuer: jwtSettings.issuer,
+    audience: jwtSettings.audience,
+  });
 }
